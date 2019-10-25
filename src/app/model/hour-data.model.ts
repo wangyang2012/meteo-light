8f1e2dea62694e062db0ba35c8cce18cd71592b8
YWANG <wangyang1712@gmail.com>
2019-10-25 17:52:43 +0200

    add hour data explaination

--- src/app/model/hour-data.model.ts
+++ src/app/model/hour-data.model.ts
@@ -19,4 +19,40 @@ export class HourData {
     KINDEX: string;
     CAPE180_0: string;
     CIN180_0: string;
-};
+}
+
+/*
+Données horaires
+0H00
+Icône Fcst_day_0->hourly_data->0H00->ICON
+Conditions (texte) *
+Fcst_day_0->hourly_data->0H00->CONDITION
+Température [°C] Fcst_day_0->hourly_data->0H00->TMP2m
+Point de rosée [°C] Fcst_day_0->hourly_data->0H00->DPT2m
+Refroidissement éolien [°C] Fcst_day_0->hourly_data->0H00->WNDCHILL2m
+Humidité relative [%]
+Fcst_day_0->hourly_data->0H00->RH2m
+Pression atmosphérique [Hpa]
+Fcst_day_0->hourly_data->0H00->PRMSL
+Précipitations [mm]
+Fcst_day_0->hourly_data->0H00->APCPsfc
+Vitesse du vent à 10m [Km/h]
+Fcst_day_0->hourly_data->0H00->WNDSPD10m
+Rafales à 10m [Km/h] Fcst_day_0->hourly_data->0H00->WNDGUST10m
+Direction du vent [°] Fcst_day_0->hourly_data->0H00->WNDDIR10m
+Direction du vent Fcst_day_0->hourly_data->0H00->WNDDIRCARD10
+Type de précipitation [0 = pluie,
+1 = neige]
+Fcst_day_0->hourly_data->0H00->ISSNOW
+Nuages haute altitude Fcst_day_0->hourly_data->0H00->HCDC
+Nuages moyenne altitude Fcst_day_0->hourly_data->0H00->MCDC
+www.prevision-meteo.ch Données météo pour sites web
+Nuages basse altitude Fcst_day_0->hourly_data->0H00->LCDC
+Isotherme zéro degré [°C] Fcst_day_0->hourly_data->0H00->HGT0C
+K-index (potentiel orageux) Fcst_day_0->hourly_data->0H00->KINDEX
+CAPE 180-0
+Fcst_day_0->hourly_data->0H00->CAPE180_0
+CIN 180-0
+Fcst_day_0->hourly_data->0H00->CIN180_0
+
+ */
