aee85f75cec455a35d7eee7a45d5c5fa16b551ab
YWANG <wangyang1712@gmail.com>
2019-10-31 17:41:02 +0100

    add meteo-france URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: true,
     api_search_url: 'https://www.prevision-meteo.ch/ajax/search?term=',
-    api_backend_hostname: 'https://www.prevision-meteo.ch/services/json/'
+    api_backend_hostname: 'https://www.prevision-meteo.ch/services/json/',
+    api_meteo_france_cities: 'http://www.meteofrance.com/mf3-rpc-portlet/rest/lieu/facet/previsions/search/94370',
+    api_meteo_france_prevision: 'http://www.meteofrance.com/previsions-meteo-france/sucy-en-brie/94370'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,9 @@
 export const environment = {
   production: false,
   api_search_url: 'https://www.prevision-meteo.ch/ajax/search?term=',
-  api_backend_hostname: 'https://www.prevision-meteo.ch/services/json/'
+  api_backend_hostname: 'https://www.prevision-meteo.ch/services/json/',
+    api_meteo_france_cities: 'http://www.meteofrance.com/mf3-rpc-portlet/rest/lieu/facet/previsions/search/94370',
+    api_meteo_france_prevision: 'http://www.meteofrance.com/previsions-meteo-france/sucy-en-brie/94370'
 };
 
 /*
